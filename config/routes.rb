dd938b8ad080630657ab97ca55c50de692f4b8ae
Helene Delachapelle <helene.delachapelle@edhec.com>
2022-01-26 12:07:53 +0100

    controllers, links between models and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bookmarks
-  resources :lists
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
   resources :movies, only: [ :index, :show, :search ]
 end
